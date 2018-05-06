7154eeccddefc2228b647a1afc1d30b32a40b9d1
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-05-06 15:18:28 -0700

    Pseudocoded out elements of each function.

--- ATMProject.cpp
+++ ATMProject.cpp
@@ -3,10 +3,12 @@
 
 using namespace std;
 
-void topMenu()
+void topMenu(bool loggedIn)
 {
-  // Basic Menu displayed
+  // 1) Basic Menu displayed
+  // 2) User must log in to access functions
   int option = 0;
+  loggedIn = false;
   cout
   << "1. Sign in"
   << endl
@@ -17,10 +19,61 @@ void topMenu()
   << "4. Withdraw"
   << endl
   << "5. Exit"
-  return 0;
+
 }
-int main()
+
+bool goodbye(bool loggedIn)
 {
 
+  // 1) Displays Good Bye exits returns logged in bool false
+  // 2) Starts topMenu
+  return loggedIn;
+}
+
+// Sign In function
+bool signIn(bool loggedIn)
+{
+  // 1) Holds accounts for only 10 people in Array
+  // 2) All have starting balance of $1000
+  // 3) User types in an account number from 0 to 10 to access balance
+  // 4) incorrect number or attempt to access other options without signing in
+  // returns logged in bool false then runs top menu
+  return loggedIn;
+}
+
+// Balance
+void balance()
+{
+  // 1) Display current balance
+  // 2) Diplay "low balance" For low balance (if below $100)
+  // 3) Run top menu with bool logged in true
+}
+
+// Deposit
+double deposit(double depositedAmount)
+{
+  // 1) Prompt user for amount to deposit
+  // 2) If input is negative count updated max 3 warns user invalid amount
+  // then tuns top menu
+  // 3) If successful run update balance
+  return depositedAmount;
+}
+
+// Withdraw
+double withdraw(double withdrawnAmount)
+{
+  // 1) Prompt user to enter amount to withdraw
+  // 2) Checks if user has enough then subtracts amount
+  // 3) Or displays warning and returns to top menu
+  return withdrawnAmount;
+}
+
+// Runs top menu functions
+int main()
+{
+  cout
+  << "Welcome to Luigi's ATM service~"
+  << endl;
+  topMenu();
   return 0;
 }
