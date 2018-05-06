8dcd6565d9146e213ef4a0761c3eb3755a7067db
BYRON CAMPBELL <blcampbell@alumni.cpp.edu>
2018-05-06 14:19:55 -0700

    Initial files for my project

---
+++ ATMProject.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  // Basic Menu displayed
+  int option = 0;
+  cout
+  << "1. Sign in"
+  << endl
+  << "2. Balance"
+  << endl
+  << "3. Deposit"
+  << endl
+  <<
+  return 0;
+}
